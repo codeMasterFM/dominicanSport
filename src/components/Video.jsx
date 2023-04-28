9b2ea48677ce7867b6d3ae08812c6f3071de5da2
codeMasterFM <Franklynp87@gmail.com>
2023-04-28 08:32:13 +0800

    neevo cambio de ruta

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import iframe from '../iframe.json'
 import "../css/video.css";
 
 const Video = () => {
-const[getUrl,setState]=useState('https://mega.nz/embed/oH1CnDqA#ruAbDoEHDzBFC-O1J7_MFBpY91z-h0xDbzmTbtqjl-M')
+const[getUrl,setState]=useState('https://nbamlb.live/canales/hls.php?url=aHR0cHM6Ly9nbWIyLmxpdmVtb2RvLmNsaWNrL3VnMVhGQ3R6ZmVCelZuRUVnbUN6SUEvMTY4MjY2ODMyMS8zMjM4MzAzMDNhNjUzMjNhMzE2MTMwMzAzYTM5MzYzMzYzM2E2NjM2MzAzNTNhNjUzOTYxMzAzYTM0Mzg2NDM2M2E2NjY1MzYzOS9mZjEwMS9wbGF5bGlzdC5tM3U4P2I9MQ==')
   useEffect(()=>{
 setState(iframe.url)
   },[])
