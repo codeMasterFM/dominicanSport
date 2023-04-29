40401d4604a63bd83ad5651191f07abbde8412b9
codeMasterFM <Franklynp87@gmail.com>
2023-04-29 08:30:49 +0800

    cambio de ruta

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -3,7 +3,7 @@ import iframe from '../iframe.json'
 import "../css/video.css";
 
 const Video = () => {
-const[getUrl,setState]=useState('https://nbamlb.live/canales/hls.php?url=aHR0cHM6Ly9nbWIyLmxpdmVtb2RvLmNsaWNrL3VnMVhGQ3R6ZmVCelZuRUVnbUN6SUEvMTY4MjY2ODMyMS8zMjM4MzAzMDNhNjUzMjNhMzE2MTMwMzAzYTM5MzYzMzYzM2E2NjM2MzAzNTNhNjUzOTYxMzAzYTM0Mzg2NDM2M2E2NjY1MzYzOS9mZjEwMS9wbGF5bGlzdC5tM3U4P2I9MQ==')
+const[getUrl,setState]=useState('https://nbamlb.live/canales/hls.php?url=aHR0cHM6Ly9nbWIxLmxpdmVtb2RvLmNsaWNrLzBwd2YxOEZyS2RkanJacUFFeGlaWncvMTY4Mjc1NDcwMC8zMjM4MzAzMDNhNjUzMjNhMzE2MTMwMzAzYTM5MzYzMzYzM2E2NjM2MzAzNTNhNjUzOTYxMzAzYTM0Mzg2NDM2M2E2NjY1MzYzOS9mZjExNi9wbGF5bGlzdC5tM3U4P2I9MQ==')
   useEffect(()=>{
 setState(iframe.url)
   },[])
@@ -11,7 +11,7 @@ setState(iframe.url)
   return <div className="VideoContainer">
     <h2>Estream </h2>
     <p>Aqui se muestran los partidos en vivo</p>
-    <iframe src={getUrl} width="640" height="360" allowFullScreen="true" allowfullscreen="true" allow="autoplay">hola</iframe>
+    <iframe src={getUrl} width="640" height="400" allowFullScreen="true" allowfullscreen="true" allow="autoplay">hola</iframe>
   </div>;
 };
 
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,7 +3,6 @@ import Nav from '../components/Nav'
 import ListPost from '../components/ListPost'
 import Video from '../components/Video'
 import '../css/homecontainer.css'
-import Adds from '../components/Adds';
 
 export const Home = () => {
   return (
@@ -15,7 +14,6 @@ export const Home = () => {
         <ListPost/>
         </div>
         <div className='Homeadds'>
-         <Adds/>
         </div>
         </div>
     </div>
