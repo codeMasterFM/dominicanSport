c7de85ff0d4c44be15bafb8547c8bde8c0e7130b
codeMasterFM <Franklynp87@gmail.com>
2023-05-01 04:06:48 +0800

    cambio de ruta f

--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -11,7 +11,7 @@ setState(iframe.url)
   return <div className="VideoContainer">
     <h1>juego de pelota en vivo hoy</h1>
     <p>Aqui se muestran los partidos en vivo</p>
-    <iframe src={getUrl} width="670" height="400" allowFullScreen="true" allowfullscreen="true" allow="autoplay">hola</iframe>
+    <iframe allow="encrypted-media" src="https://clarovideo.auronplaylive.repl.co/?get=aHR0cHM6Ly9sYXRhbXZvc2xpdmVjbGFyb3ZpZGVvLmFrYW1haXplZC5uZXQvQ29udGVudC9EQVNIX0RBU0hfRksvTGl2ZS9DaGFubmVsKEZPWFNQT1JUU19QUkVNSVVNX0hEKS9tYW5pZmVzdC5tcGQ=&key=OTFhMjFmN2ZmYzAzYjNlNDZmZTM0OGRkMDc1NTc1OGU=&key2=NWZlNjBmZDc3ZGZhNjFkYmY0MWQxYTBhYzk1OTYyMWQ=&aut=true&lang=0" width="670" height="400" allowFullScreen="true" allowfullscreen="true" >hola</iframe>
   </div>;
 };
 
